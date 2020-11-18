d1b93aa3c76d0857d4452e327914e9a64aa63782
HoopDead <polec0@wp.pl>
2020-11-18 14:44:48 +0100

    Zadanie 3 cale

--- lista4/zadanie3.py
+++ lista4/zadanie3.py
@@ -6,4 +6,11 @@ m = 13.3 * 10**3 # Szybkosc zuzycia paliwa (kg/s)
 g = 9.81 # Przyspieszenie ziemskie (m/s^2)
 
 def v(t):
-    return u * np.log(M0/(M0 - (m*t))) - g*t
+    return u * np.log(M0/(M0 - (m*t))) - g*t 
+
+t = np.linspace(0, 100, 2000) # Stworzenie dwóch tysięcy liczb w przedziale (0, 100)
+
+for time in t:
+    if v(time) >= 335:
+         print(f"Czas potrzebny do osiagniecia predkosci 335 {time} sekund") 
+         break
