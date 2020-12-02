deacab2c6317b70824220d48e32eb4a08f76c45e
HoopDead <polec0@wp.pl>
2020-12-02 14:19:45 +0100

    Zadanie 5 lista 4

--- lista4/zadanie5.py
+++ lista4/zadanie5.py
@@ -1,5 +1,6 @@
 from scipy import optimize
 import numpy as np
+import math as m
 
 # x = 10
 # y = 3
@@ -13,18 +14,22 @@ import numpy as np
 # v0 * 0.70710678118 * t = 10
 # v0 * 0.70710678118 * t - t^2 * 4.095 = 1
 
+g = 9.81
+
 def f(param_in):
-    v0, t = param_in
-    f_1 = v0 * 0.70710678118 * t - 10
-    f_2 = v0 *  0.70710678118 * t - t**2 * 4.095 - 1
+    v0, angle = param_in
+    t = 10 / (v0 * np.cos(angle))
+    f_1 = v0 * np.sin(angle) * t - ((g * t**2)/2) - 1
+    f_2 = v0 * (np.sin(angle) + np.cos(angle)) - (g*t)
     return [f_1, f_2]
 
 if __name__ == "__main__":
     ans = list()
-    for v0 in np.arange(0, 25, 0.1):
-        for t in np.arange(0, 10, 0.1):
-            root = optimize.fsolve(f, [v0, t])
-            if [round(root[0], 3), round(root[1], 3)] not in ans and round(root[0], 3) >= 1:
+    for v0 in np.arange(0.1, 25, 0.1):
+        for angle in np.arange(1, 90, 1):
+            root = optimize.fsolve(f, [v0, angle])
+            if [round(root[0], 3), round(root[1], 3)] not in ans:
                 ans.append([round(root[0], 3), round(root[1], 3)])
 
-print(f"Potrzebna predkosc to {ans[0][0]}")
+
+print(f"Potrzebna predkosc to {ans[0][0]} m/s, potrzebny kat poczatkowy to {ans[0][1]} stopni")
