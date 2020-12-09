b7b83d5941408ffdf2eac0bc32cb1eadbf60270f
HoopDead <polec0@wp.pl>
2020-12-09 13:22:13 +0100

    Lista 3 zadanie 5

--- lista4/zadanie5.py
+++ lista4/zadanie5.py
@@ -26,10 +26,12 @@ def f(param_in):
 if __name__ == "__main__":
     ans = list()
     for v0 in np.arange(0.1, 25, 0.1):
-        for angle in np.arange(1, 90, 1):
-            root = optimize.fsolve(f, [v0, angle])
+        for angle in np.arange(1, 51, 1):
+            root = optimize.fsolve(f, [10.431, m.radians(angle)])
             if [round(root[0], 3), round(root[1], 3)] not in ans:
                 ans.append([round(root[0], 3), round(root[1], 3)])
 
+# W momencie, w którym mamy za 10.431 podstawione v0 to wychodzi nam 10.431 m/s, natomiast kat wychodzi 346730 stopni, w zwiazku z tym analitycznie zadaje v0 rowne 10.431 i sprawdzam tylko kat.
 
-print(f"Potrzebna predkosc to {ans[0][0]} m/s, potrzebny kat poczatkowy to {ans[0][1]} stopni")
+
+print(f"Potrzebna predkosc to {ans[0][0]} m/s, potrzebny kat poczatkowy to {m.degrees(ans[0][1])} stopni")
