f2232b7591ea34af682eef85d3dd949ae876a46a
HoopDead <polec0@wp.pl>
2020-12-16 14:18:24 +0100

    Zadanie 1, 2 lista 5

--- lista5/zadanie1.py
+++ lista5/zadanie1.py
@@ -1,20 +1,7 @@
-from scipy import interpolate
-import matplotlib.pyplot as plt
-import numpy as np
-
-# y = 1.225, 0.905, 0.652
-# x = 0,     3,     6
+from scipy.interpolate import lagrange
 
 x = [0, 3, 6]
 y = [1.225, 0.905, 0.652]
 
-f = interpolate.interp1d(x, y, kind = 'quadratic')
-
-
-xnew = np.arange(0, 6, 0.01)
-ynew = f(xnew)   # use interpolation function returned by `interp1d`
-
-plt.plot(x, y, 'o', xnew, ynew, '-')
-plt.xlabel("h (km)")
-plt.ylabel("p (kg/m^3)")
-plt.show()
+ans = lagrange(x, y)
+print(ans)
--- lista5/zadanie2.py
+++ lista5/zadanie2.py
@@ -1,4 +1,5 @@
 from scipy import interpolate
+from scipy.misc import derivative
 
 # x = 1, 1.25, 1.75, 2, 2.25, 2.5, 2.75, 3
 # y = -0.5403, -0.0104, 0.9423, 1.7445, 1.3073, -0.7718, -2.4986, -0.7903, 2.7334
@@ -8,4 +9,4 @@ y = [-0.5403, -0.0104, 0.9423, 1.7445, 1.3073, -0.7718, -2.4986, -0.7903, 2.7334
 
 spline = interpolate.InterpolatedUnivariateSpline(x, y)
 print(f"Miejsca zerowe dla zadanej funkcji to: {spline.roots()}")
-spline_d = spline.derivative()
+print(f"Wartosc w punkcie x = 2.1 to {derivative(spline, 2.1)}")
