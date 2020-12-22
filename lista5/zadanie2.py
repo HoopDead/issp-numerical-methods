8aff870cae24f0c7f2378d4f61c3894fa5abbc41
HoopDead <polec0@wp.pl>
2020-12-22 15:14:30 +0100

    Zadanie 2 lista 5

--- lista5/zadanie2.py
+++ lista5/zadanie2.py
@@ -1,12 +1,11 @@
-from scipy import interpolate
-from scipy.misc import derivative
+import numpy as np
+from scipy.interpolate import Akima1DInterpolator
 
-# x = 1, 1.25, 1.75, 2, 2.25, 2.5, 2.75, 3
-# y = -0.5403, -0.0104, 0.9423, 1.7445, 1.3073, -0.7718, -2.4986, -0.7903, 2.7334
 
 x = [1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3]
 y = [-0.5403, -0.0104, 0.9423, 1.7445, 1.3073, -0.7718, -2.4986, -0.7903, 2.7334]
 
-spline = interpolate.InterpolatedUnivariateSpline(x, y)
-print(f"Miejsca zerowe dla zadanej funkcji to: {spline.roots()}")
-print(f"Wartosc w punkcie x = 2.1 to {derivative(spline, 2.1)}")
+spl = Akima1DInterpolator(x, y)
+
+print(f"Miejsca zerrowe to: {spl.roots()}")
+print(f"Wartosc pochodnej w x = 2.1 to {spl(2.1, 1)}")
