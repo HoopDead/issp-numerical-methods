143054b5e20a95d0178d9e857fb252f6550cc888
HoopDead <polec0@wp.pl>
2021-01-31 17:11:12 +0100

    Egzamin updated

--- egzamin/egzamin.py
+++ egzamin/egzamin.py
@@ -7,16 +7,26 @@ def f(u, x):
     return (u[1], -11*u[1] -24*u[0])
 
 y0 = [0, -7]
-xs = np.linspace(0, 1, 200)
-us = odeint(f, y0, xs)
-ys = us[:, 0]
-plt.plot(xs, ys, '-')
+ts = np.linspace(0, 1, 200)
 
-list_of_points = dict(zip(ys, xs))
+index = 1
+
+us = odeint(f, y0, ts)
+ys = us[:, 0]
+plt.plot(ts, ys, '-')
+
+list_of_points = dict(zip(ys, ts))
 
 for y, x in list_of_points.items():
-    print(f"Punkt na osi y: {y}, punkt na osi x: {x}")
+    print(f"Numer punktu: {index}, punkt na osi y (wartość): {y}, punkt na osi x (argument): {x}")
+    index += 1
 
-a = argrelextrema(ys, np.less)
-plt.plot(xs[a[0]], ys[a[0]], 'r*')
+extrema_index = argrelextrema(ys, np.less)[0]
+plt.plot(ts[extrema_index], ys[extrema_index], 'r*')
+print(f"Punkt, w jakim znajduje sie ekstremum, na osi y: {ys[extrema_index]}, na osi x: {ts[extrema_index]}")
+plt.grid()
+plt.title("Wykres funkcji y'' + 11y' + 24y = 0 ")
+plt.xlabel("Argumenty t")
+plt.ylabel("Wartości y")
+plt.legend(["y'' + 11y' + 24y", "Ekstremum"])
 plt.show()
